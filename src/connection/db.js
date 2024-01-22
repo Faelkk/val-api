94898f0134fae98fd4081cbf457031327504994c
Rafael Achtenberg <achten.rafael16@gmail.com>
2024-01-21 22:34:29 -0300

    feat: add enviroment variables for db connection

--- src/connection/db.js
+++ src/connection/db.js
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
 const supabase = createClient(
-  "https://gqnzfpvcfsvoxflptsjr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxbnpmcHZjZnN2b3hmbHB0c2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU3Njk2MDAsImV4cCI6MjAyMTM0NTYwMH0.koUprwjRoS-s71ZV1i-PwvJ2qfkSEv8-3hM8_HJk1RU"
+  process.env.API_KEY_URL,
+  process.env.API_PASSWORD_URL
 );
 
 module.exports = supabase;
